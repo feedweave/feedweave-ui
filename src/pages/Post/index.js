2f81d44846418f42583c7f8adbedfaa6ee993eaa
Denis Nazarov <d@denisnazarov.com>
2020-02-20 21:16:23 -0500

    wip meta tags

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Helmet from "react-helmet";
 
 import { API_HOST } from "../../util";
 import { PostFeedItem } from "../../components/PostFeed";
@@ -15,6 +16,30 @@ const loadComments = async txId => {
   const res = await fetch(`${API_HOST}/transaction/${txId}/comments`);
   const { users, comments } = await res.json();
   return { users, comments };
+};
+
+const PostMetaTags = ({ post }) => {
+  const { title, description, image, url, username } = extractPostMeta(post);
+  return (
+    <Helmet>
+      <title>{`${title} | FEEDweave`}</title>
+      <meta
+        property="og:description"
+        content="Offering tour packages for individuals or groups."
+      />
+      <meta
+        property="og:image"
+        content="http://euro-travel-example.com/thumbnail.jpg"
+      />
+      <meta
+        property="og:url"
+        content="http://euro-travel-example.com/index.htm"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="og:site_name" content="FEEDweave" />
+      <meta name="twitter:site" content="@FEEDweave_" />
+    </Helmet>
+  );
 };
 
 class Post extends React.Component {
