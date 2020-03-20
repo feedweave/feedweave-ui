b8ff05c6ab0be4ae8a41e199f840d8c08a1d43d9
Denis Nazarov <d@denisnazarov.com>
2020-03-20 13:47:29 -0400

    feed pagination

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { Link } from "@reach/router";
+import { Button } from "reactstrap";
 
-import { API_HOST, APP_NAME, UserContext } from "../../util";
+import { API_HOST, UserContext } from "../../util";
 import PostFeed from "../../components/PostFeed";
 
 import styles from "./index.module.css";
+
+const fetchFeed = async cursor => {
+  const queryParam = cursor ? `?cursor=${cursor}` : "";
+  const res = await fetch(`${API_HOST}/post-feed${queryParam}`);
+  const json = await res.json();
+
+  return json;
+};
 
 class Home extends React.Component {
   constructor(props) {
@@ -12,21 +21,40 @@ class Home extends React.Component {
     this.state = {
       error: null,
       isLoaded: false,
+      nextCursor: null,
       feed: {}
     };
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
   static contextType = UserContext;
 
+  async handleClick() {
+    const {
+      nextCursor,
+      feed: { transactions, users }
+    } = this.state;
+    const json = await fetchFeed(nextCursor);
+    this.setState({
+      isLoaded: true,
+      feed: {
+        transactions: transactions.concat(json.transactions),
+        users: users.concat(json.users)
+      },
+      nextCursor: json.nextCursor
+    });
+  }
+
   async componentDidMount() {
     // TODO handle errors
-    const res = await fetch(`${API_HOST}/transactions?app-name=${APP_NAME}`);
-    const json = await res.json();
-    this.setState({ isLoaded: true, feed: json });
+    const json = await fetchFeed();
+    const { nextCursor } = json;
+    this.setState({ isLoaded: true, feed: json, nextCursor });
   }
 
   render() {
-    const { isLoaded, feed } = this.state;
+    const { isLoaded, feed, nextCursor } = this.state;
     const { user } = this.context;
     return (
       <div>
@@ -46,7 +74,21 @@ class Home extends React.Component {
             </ul>
           )}
         </nav>
-        {isLoaded ? <PostFeed feed={feed} /> : "Loading..."}
+        {isLoaded ? (
+          <div>
+            <PostFeed feed={feed} />
+            {nextCursor ? (
+              <Button
+                className={styles.loadMoreButton}
+                onClick={this.handleClick}
+              >
+                Load more posts
+              </Button>
+            ) : null}
+          </div>
+        ) : (
+          "Loading..."
+        )}
       </div>
     );
   }
